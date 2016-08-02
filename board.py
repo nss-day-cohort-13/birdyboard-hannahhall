b29895e55c1b3f84dbae5b91da8e47e8649ca774
Hannah Hall <hannahkhall13@gmail.com>
2016-08-02 10:54:52 -0500

    testing and code for writing a private chirp

--- board.py
+++ board.py
@@ -1,9 +1,11 @@
+import random
+
 class Birdyboard():
     def __init__(self):
         self.users = list()
         self.currentUser = None
         self.publicChirps = list()
-
+        self.privateChirps = dict()
     def menu(self):
         print("""
 <<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>
@@ -23,30 +25,52 @@ class Birdyboard():
             self.newUser(name, username)
             self.menu()
         elif choice == '2':
-            userLength = len(self.users)
-            counter = 1
-            index = 0
-            while userLength:
-                print(str(counter) + '. ' + self.users[index][1])
-                counter += 1
-                index += 1
-                userLength -= 1
+            printAllUsers()
             userChoice = input('Select User \n> ')
             self.selectUser(int(userChoice))
             self.menu()
+        elif choice == '4':
+            chirp = input('Create new public chirp: \n> ')
+            self.newPublicChirp(message)
+        elif choice == '5':
+            printAllUsers()
+            recipient = input('Choose who to send private chirp to: \n> ')
+            message = input('Enter Chirp: \n> ')
+            self.newPrivateChirp(message, recipient)
         elif choice == '6':
             exit()
+
+    def printAllUsers():
+        userLength = len(self.users)
+        counter = 1
+        index = 0
+        while userLength:
+            print(str(counter) + '. ' + self.users[index][1])
+            counter += 1
+            index += 1
+            userLength -= 1
 
     def newUser(self, name, username):
         self.users.append((name, username))
 
     def selectUser(self, choice):
         index = choice - 1
-        self.currentUser = self.users[index]
-        return self.currentUser
+        if not self.currentUser:
+            self.currentUser = self.users[index]
+            return self.currentUser
+        else:
+            self.privateRecip = self.users[index]
+            return self.privateRecip
 
     def newPublicChirp(self, message):
         self.publicChirps.append({self.currentUser[1]: message})
 
-# board = Birdyboard()
-# board.menu()
+    def newPrivateChirp(self, message, recipient):
+        key = random.randint(1, 1000)
+        self.privateChirps[key] = []
+        self.privateChirps[key].append({key, self.currentUser[1], recipient[1]})        
+        self.privateChirps[key].append((self.currentUser[1], message))
+
+if __name__ == '__main__':
+    board = Birdyboard()
+    board.menu()
--- testboard.py
+++ testboard.py
@@ -6,9 +6,11 @@ class Testboard(unittest.TestCase):
     def setUpClass(self):
         self.board = Birdyboard()
         self.board.newUser('name', 'username')
+        self.board.newUser('gary', 'gary101')
 
     def test_new_user(self):
         self.assertIn(('name', 'username'), self.board.users)
+
     def test_select_user(self):
         input = 1
         self.board.selectUser(input)
@@ -16,10 +18,22 @@ class Testboard(unittest.TestCase):
 
     def test_new_public_chirp(self):
         self.board.selectUser(1)
-        print(self.board.currentUser)
-        newMessage = "Hello World"
-        self.board.newPublicChirp(newMessage)
+        newchirp = "Hello World"
+        self.board.newPublicChirp(newchirp)
         self.assertIn({'username': 'Hello World'}, self.board.publicChirps)
+
+    def test_new_private_chirp(self):
+        # creating current user
+        self.board.selectUser(1)
+        # choosing recipient of message
+        self.board.selectUser(2)
+        newChirp = "Hello gary!"
+        self.board.newPrivateChirp(newChirp, self.board.privateRecip)
+        for key, value in self.board.privateChirps.items():
+            self.assertEqual([{'username', 'gary101', key}, ('username', 'Hello gary!')], value)
+
+    def test_chirp_list_view(self):
+        pass
 
 
 
