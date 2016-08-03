00ec0376d1bc769acb44ef9272a283aad61251ac
Hannah Hall <hannahkhall13@gmail.com>
2016-08-03 14:19:19 -0500

    changes for adding new classes

--- board.py
+++ board.py
@@ -1,19 +1,23 @@
 import random
 import pickle
+from user import *
+from chirp import *
 
-class Birdyboard():
-    def __init__(self):
+class Birdyboard:
+    def __init__(self, boardfile, userfile):
         self.currentUser = None
         self.privateRecip = None
         self.userPrivateChirps = dict()
+        self.boardfile = boardfile
+        self.userfile = userfile
         try:
-            self.deserializeChirps('test_board.txt')
+            self.deserializeChirps(boardfile)
         except EOFError:
             self.chirps = dict()
             self.chirps['private'] = dict()
             self.chirps['public'] = dict()
         try:
-            self.deserializeUsers('test_users.txt')
+            self.deserializeUsers(userfile)
         except EOFError:
             self.users = list()
 
@@ -74,7 +78,8 @@ class Birdyboard():
             self.menu()
         elif choice == '5':
             self.printAllUsers()
-            recipient = input('Choose who to send private chirp to: \n> ')
+            userchoice = input('Choose who to send private chirp to: \n> ')
+            recipient = self.selectUser(int(userchoice))
             message = input('Enter Chirp: \n> ')
             self.newPrivateChirp(message, recipient)
             self.menu()
@@ -85,64 +90,60 @@ class Birdyboard():
         print(self.users)
         userLength = len(self.users)
         counter = 1
-        index = 0
-        while userLength:
-            print(str(counter) + '. ' + self.users[index][1])
+        for user in self.users:
+            print(str(counter) + '. ' + user.username)
             counter += 1
-            index += 1
-            userLength -= 1
 
     def newUser(self, name, username):
-        self.users.append((name, username))
-        self.serializeUsers('test_users.txt')
+        existingUsers = []
+        for user in self.users:
+            existingUsers.append(user.username)
+        if not username in existingUsers:
+            user = User(name, username)
+            self.users.append(user)
+            self.serializeUsers(self.userfile)
 
     def selectUser(self, choice):
         index = choice - 1
         if not self.currentUser:
             self.currentUser = self.users[index]
+            print(self.currentUser.username)
             return self.currentUser
         else:
             self.privateRecip = self.users[index]
+            print(self.privateRecip.username)
             return self.privateRecip
 
     def newPublicChirp(self, message):
         key = random.randint(1, 1000)
-        self.chirps['public'][key] = []
-        self.chirps['public'][key].append({self.currentUser[1]: message})
-        self.serializeMessages('test_board.txt')
+        self.chirps['public'][key] = PublicMessage(self.currentUser.username, message)
+        self.serializeMessages(self.boardfile)
 
     def newPrivateChirp(self, message, recipient):
         key = random.randint(1, 1000)
-        self.chirps['private'][key] = []
-        self.chirps['private'][key].append([self.currentUser, self.privateRecip])
-        self.chirps['private'][key].append({self.currentUser[1]: message})
-        self.serializeMessages('test_board.txt')
+        self.chirps['private'][key] = PrivateMessage(self.currentUser.username, self.privateRecip.username, message)
+        self.serializeMessages(self.boardfile)
 
 
     def showAllChirps(self):
         self.findPrivateChirps()
-        privateLength = len(self.chirps['private'])
-        publicLength = len(self.chirps['public'])
         print('<<<<<Private Chirps>>>>>')
         counter = 1
         for key, value in self.userPrivateChirps.items():
-            message = ''.join("{!s}: {!r}".format(key,val) for (key,val) in value[-1].items())
-            print(str(counter) + '. ' + message)
+            print(str(counter) + '. ' + value.sender + ': ' + value.message)
             counter += 1
         print('<<<<<Public Chirps>>>>>')
         for key, value in self.chirps['public'].items():
-            message = ''.join("{!s}: {!r}".format(key,val) for (key,val) in value[0].items())
-            print(str(counter) + '. ' + message)
+            print(str(counter) + '. ' + value.sender + ': ' + value.message)
             counter += 1
         print(str(counter) + '. ' + 'Main Menu')
 
 
     def findPrivateChirps(self):
         for key, value in self.chirps['private'].items():
-            for info in value[0]:
-                if info == self.currentUser:
-                    self.userPrivateChirps[key] = value
+            if self.currentUser.username == value.sender or value.receiver:
+                self.userPrivateChirps[key] = value
 
 if __name__ == '__main__':
-    board = Birdyboard()
+    board = Birdyboard('board.txt', 'users.txt')
     board.menu()
--- testboard.py
+++ testboard.py
@@ -4,34 +4,35 @@ from board import *
 class Testboard(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.board = Birdyboard()
-        self.board.newUser('name', 'username')
-        self.board.newUser('gary', 'gary101')
+        self.board = Birdyboard('test_board.txt', 'test_users.txt')
 
     def test_new_user(self):
-        self.assertIn(('name', 'username'), self.board.users)
+        self.board.newUser('name', 'username')
+        self.board.newUser('gary', 'gary101')
+        length_of_users = len(self.board.users)
+        self.assertEqual(2, length_of_users)
 
     def test_select_user(self):
         input = 1
         self.board.selectUser(input)
-        self.assertEqual(self.board.currentUser, ('name', 'username'))
+        self.assertEqual(self.board.currentUser.username, 'username')
 
     def test_new_public_chirp(self):
         self.board.selectUser(1)
         newchirp = "Hello World"
+        startLength = len(self.board.chirps['public'])
         self.board.newPublicChirp(newchirp)
-        for key, value in self.board.chirps['public'].items():
-            self.assertIn({'username': 'Hello World'}, value)
+        self.assertEqual(startLength + 1, len(self.board.chirps['public']))
 
     def test_new_private_chirp(self):
         # creating current user
         self.board.selectUser(1)
         # choosing recipient of message
-        self.board.selectUser(2)
-        newChirp = "Hello gary!"
-        self.board.newPrivateChirp(newChirp, self.board.privateRecip)
-        for key, value in self.board.chirps['private'].items():
-            self.assertEqual([{'username', 'gary101', key}, ('username', 'Hello gary!')], value)
+        recipient = self.board.selectUser(2)
+        newchirp = "Hello gary!"
+        startLength = len(self.board.chirps['private'])
+        self.board.newPrivateChirp(newchirp, recipient)
+        self.assertEqual(startLength + 1, len(self.board.chirps['private']))
 
     def test_chirp_list_view(self):
         self.board.selectUser(1)
